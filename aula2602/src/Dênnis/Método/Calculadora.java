75f6078f2404de4af858bcb330ecabb61a00efc7
Dennis1334 <90472903+Dennis1334@users.noreply.github.com>
2025-02-26 22:19:32 -0300

    Add files via upload

---
+++ aula2602/src/Dênnis/Método/Calculadora.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dênnis.Método;
+
+/**
+ *
+ * @author DENNIS_3960
+ */
+public class Calculadora {
+    
+    int valor1 = 10;
+    int valor2 = 10;
+    
+    void adicao (){
+        
+        int resultado = valor1 + valor2;
+        System.out.println("O valor da soma é: " + resultado);
+    }
+    void subtracao (){
+        
+        int resultado = valor1 - valor2;
+        System.out.println("O valor da subtracao é: " + resultado);
+    }
+    void multiplicacao (){
+        
+        int resultado = valor1 * valor2;
+        System.out.println("O valor da multiplicacao é: " + resultado);
+    }
+    void divisao (){
+        
+        int resultado = valor1 / valor2;
+        System.out.println("O valor da divisao é: " + resultado);
+    }
+}
---
+++ aula2602/src/Dênnis/Método/Testar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dênnis.Método;
+
+/**
+ *
+ * @author DENNIS_3960
+ */
+public class Testar {
+    public static void main(String[] args) {
+        Calculadora x = new Calculadora();
+        x.adicao();
+    }
+    
+}
